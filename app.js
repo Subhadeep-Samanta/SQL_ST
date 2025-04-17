2c191be9f0b6a3196a8e7cdf5bfb0c081aec974f
Subhadeep-Samanta <subhadeepsamanta1535@gmail.com>
2025-04-17 06:27:24 +0530

    sql setup

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import connection from "./db.js";
-
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
 // Example query
 connection.query('SELECT * FROM users', (err, results) => {
   if (err) throw err;
   console.log(results);
 });
+
+const app=express();
+const PORT=process.env.PORT
+app.listen(PORT,()=>{
+  console.log(`server is running on port ${PORT}`);
+  
+})
